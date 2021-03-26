6efea875ee665c14f06ccd8d4efa25136aeb7950
Kate Cheng <ytcheng2013@gmail.com>
2021-03-26 15:15:54 +0800

    add list comprehension

--- review.py
+++ review.py
@@ -22,3 +22,21 @@ for d in data:
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到good')
+print(good[0])
+
+# good = [d for d in data if 'good' in d]
+# output = [(number-1) for munber in reference if number % 2 == 0]
+# 第一個d是good.append(d)中的d, 也不一定要是d, 可以是1, 就會在good清單中裝162550筆1
+
+bad =['bad' in d for d in data]
+print(bad)
+# 上下兩種寫法相等
+bad == []
+for d in data:
+	bad.append('bad' in d)
